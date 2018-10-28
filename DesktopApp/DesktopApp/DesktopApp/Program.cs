edd16a6a8cafe77279bda6966a604c49886169bb
Unknown <j-david-10@hotmail.com>
2018-10-27 20:49:54 -0600

    DesktopApp Created

---
+++ DesktopApp/DesktopApp/DesktopApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+
+namespace DesktopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist.
+            //queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage("Hello, World");
+            queue.AddMessage(message);
+
+            queue.FetchAttributes();
+
+            // Retrieve the cached approximate message count.
+            int? cachedMessageCount = queue.ApproximateMessageCount;
+
+            // Display number of messages.
+            Console.WriteLine("Number of messages in queue: " + cachedMessageCount);
+
+            Console.ReadKey();
+        }
+    }
+}
